39b01b68fd5b230d6db3238ca720cc3b0c92a56c
clean-command-1851 <ed7901979t@gmail.com>
2019-10-20 14:40:26 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,52 @@
 class Student
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+ attr_accessor :name, :grade, :id
+ 
+ @@all = []
+ 
+  def initialize(name, grade, id=nil)
+    @id = id
+    @name = name
+    @grade = grade
+  end
+ 
+  def self.create_table
+    sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        grade TEXT
+        )
+        SQL
+    DB[:conn].execute(sql) 
+  end
   
+  def self.create(name, grade)
+    students = Students.new(name, grade)
+    students.save
+    students
+  end
+  
+  def self.drop_table
+    sql= <<-SQL
+      DROP TABLE IF NOT EXISTS drop students (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        grade TEXT
+        )
+        SQL
+    DB[:conn].execute(sql) 
+  end
+ 
+  def save
+    sql = <<-SQL
+      INSERT INTO grade (name, grade) 
+      VALUES (?, ?)
+    SQL
+ 
+    DB[:conn].execute(sql, self.name, self.grade)
+ 
+  end
+ 
+end
 end
